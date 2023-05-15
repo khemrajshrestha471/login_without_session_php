588b40d36c70ac68255f4e365c4a64cdbffb1632
Khemraj Shrestha <khemrajshrestha471@gmail.com>
2023-05-15 08:41:26 +0545

    cookies added

---
+++ cookies/check_cookies.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$catg = $_COOKIE['components'];
+
+echo "Your favourite component is " . $catg;
+?>
+
+
+<!-- check the cookies set from this file as well -->
+
+
+<!-- cookies is used to store non sensitive informations only whereas session can store sensitive information -->
---
+++ cookies/cookies.php
@@ -0,0 +1,13 @@
+<?php
+
+echo "hello there";
+
+setcookie("components", "football", time() + 86000, "/");
+
+echo "<br> The cookie is set";
+
+?>
+
+
+
+<!-- in inspect > network > cookies > we can see the cookies -->
